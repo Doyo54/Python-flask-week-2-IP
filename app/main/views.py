81312592b9a5b950336c6acffbafc7495419fb7b
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-03 23:46:10 +0300

    ft: Route created in views.py that calls the request function

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from app import app
-from ..request import news,news_source
+from ..request import news,news_source,search
 
 # Views
 @app.route('/')
@@ -10,7 +10,12 @@ def index():
     View root page function that returns the index page and its data
     '''
     new = news()
-    return render_template('index.html', news = new)
+    search_movie = request.args.get('name')
+
+    if search_movie:
+        return redirect(url_for('search_news',name=search_movie))
+    else:
+     return render_template('index.html', news = new)
 
 @app.route('/sources')
 def source():
@@ -21,5 +26,12 @@ def source():
     source = news_source()
     return render_template('source.html', source = source)
 
+@app.route('/search/<name>')
+def search_news(name):
+    title = f'search results for {name}'
+    search_name_list = name.split(" ")
+    search_name_format = "+".join(search_name_list)
+    searched_news = search(search_name_format)
+    return render_template('search.html',new = searched_news,title=title)
 
 

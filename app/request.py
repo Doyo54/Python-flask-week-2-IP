ab717bddae681e876560fe8e30919df38b93ce79
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-03 21:27:36 +0300

    ft: Function created called news_source

--- app/request.py
+++ app/request.py
@@ -3,13 +3,14 @@ from .models import News
 import datetime
 from datetime import datetime, timezone
 
-base_url = 'https://newsapi.org/v2/everything?q=Apple&from=2022-05-03&sortBy=popularity&apiKey=f571903dfdd44dc9b15e9e68d3cc9e88'
+base_url = 'https://newsapi.org/v2/everything?domains={}&apiKey=f571903dfdd44dc9b15e9e68d3cc9e88'
+sources_url = 'https://newsapi.org/v2/top-headlines/sources?apiKey=f571903dfdd44dc9b15e9e68d3cc9e88'
 def news():
     '''
     Function that gets the json response to our url request
     '''
-   
-    with urllib.request.urlopen(base_url) as url:
+    get_base_url =base_url.format('bbc.co.uk')
+    with urllib.request.urlopen(get_base_url) as url:
         get_news_data = url.read()
         get_news_response = json.loads(get_news_data)
 
@@ -44,5 +45,22 @@ def process_results(news_list):
 
         news_object = News(content,author,img,date, url)
         news_results.append(news_object)
+
+    return news_results
+def news_source():
+    '''
+    Function that gets the json response to our url request
+    '''
+   
+    with urllib.request.urlopen(sources_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['sources']:
+            news_results_list = get_news_response['sources']
+            news_results = process_results(news_results_list)
+
 
     return news_results

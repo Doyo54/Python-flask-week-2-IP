e189d1778ae442611f7e5603167818751b712837
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-04 00:06:18 +0300

    ft: Second test created to test the Source class

--- app/news_test.py
+++ app/news_test.py
@@ -15,5 +15,19 @@ class NewsTest(unittest.TestCase):
     def test_instance(self):
         self.assertTrue(isinstance(self.new_news,News))
 
+class SourceTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Source class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_source = Source('Doyo','Python must be crazy','Kenya','https://www.theverge.com/2022/4/8/23016553/block-blockstream-tesla-solar-bitcoin-mine-texas')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source,Source))
+
 if __name__ ==  '__main__':
     unittest.main()

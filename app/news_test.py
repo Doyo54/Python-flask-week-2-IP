6a8752da6b6a3fe28efffef72a954ece2b0c71be
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-04 00:03:13 +0300

    ft: Test for News class created

---
+++ app/news_test.py
@@ -0,0 +1,19 @@
+import unittest
+from models import News,Source
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the News class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_news = News('Python Must Be Crazy','Doyo','https://cdn.vox-cdn.com/thumbor/OYrvaaOHBuEpdTeRO55nZnZdexs=/0x215:3000x1786/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/8937281/acastro_170726_1777_0007_v2.jpg','8/05/22','https://www.theverge.com/2022/4/8/23016553/block-blockstream-tesla-solar-bitcoin-mine-texas')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,News))
+
+if __name__ ==  '__main__':
+    unittest.main()

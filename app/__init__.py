b68f44bf1a22d11631e081082fcabfcda48a7006
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-03 12:53:06 +0300

    ft: Bootstrap added

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from config import DevConfig
+from flask_bootstrap import Bootstrap
 # Initializing application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
 
-
+app.config.from_object(DevConfig)
+bootstrap = Bootstrap(app)
 from app.main import views

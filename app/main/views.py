80d58e255a32529f6dc8a121b98ac2e39bd3a3a5
Doyo Abdikadir <doyo.doyo@student.moringaschool.com>
2022-05-03 21:30:58 +0300

    ft: New route for sources created in views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from ..request import news
+from ..request import news,news_source
 
 # Views
 @app.route('/')
@@ -11,3 +11,12 @@ def index():
     '''
     new = news()
     return render_template('index.html', news = new)
+
+@app.route('/sources')
+def source():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+    source = news_source()
+    return render_template('source.html', source = source)
